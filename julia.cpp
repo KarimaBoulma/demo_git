15017e13cffd7e9f27d0cb02ae7ff342bf0c4de4
boulma karima <boulmakarima@gmail.com>
2017-05-28 13:24:01 +0200

    calcule de la fractale

--- julia.cpp
+++ julia.cpp
@@ -10,3 +10,39 @@ g++ -Wall opencv_exempleCPP.cpp -lopencv_core -lopencv_highgui -lopencv_imgproc 
 #include <time.h>
 #include <fstream>
 
+
+
+
+
+
+
+
+
+
+
+// fractale
+
+void *CalculFractaleJulia(void *arg){
+std::complex<long double> c (reel,imaginaire);
+  for(int x=0;x<LARG;x++){
+      for(int y=0;y<HAUT;y++){
+        long double newX=((long double)((long double)x-((long double)((LARG)-X)/2))/((long double)((LARG)-X)/2));
+        long double newY=((long double)((long double)y-((long double)((HAUT)-Y)/2))/((long double)((HAUT)-Y)/2));
+        std::complex<long double> point(newY,newX);
+        int iteration=0;
+        long double norm = std::norm(point);
+        while(iteration<ITER && norm<V){
+          point=(point*point)+c;
+          norm = std::norm(point);
+          iteration++;
+        }
+        if(iteration==ITER && norm<V){
+          image.at<Vec3b>(x,y)[0]=0; 
+          image.at<Vec3b>(x,y)[1]=0; 
+          image.at<Vec3b>(x,y)[2]=0;
+        }
+        else{
+          int couleur=(int)((float)iteration/(float)ITER*255);
+unsigned char brillance=150; 
+          unsigned char saturation=150; 
+          unsigned char r,g,b;
