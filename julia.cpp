1675becb3b624e1e8f1b441cdd92738e3e4cd4f3
boulma karima <boulmakarima@gmail.com>
2017-05-28 13:12:40 +0200

    declaration de libs

---
+++ julia.cpp
@@ -0,0 +1,12 @@
+/* 
+g++ -Wall opencv_exempleCPP.cpp -lopencv_core -lopencv_highgui -lopencv_imgproc -std=c++11 
+*/
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <complex>
+#include <time.h>
+#include <fstream>
+
